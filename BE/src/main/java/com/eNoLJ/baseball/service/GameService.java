2dc4e2aa3ac61794ac256fb27f728b459307b1d8
jeong-inho <derosatam76@gmail.com>
2021-05-06 15:12:25 +0900

    [#13] Feat: GameController 클래스에 viewMembers 메소드 생성
    팀별 선수들의 정보를 보내주기 위한 viewMembers 메소드 생성, 그에 대한 비지니스 로직을 처리할 getMembersByTeamName 메소드를 GameService 클래스에 생성, 멤버들의 정보를 담을 MemberDTO 클래스 생성

--- BE/src/main/java/com/eNoLJ/baseball/service/GameService.java
+++ BE/src/main/java/com/eNoLJ/baseball/service/GameService.java
@@ -1,6 +1,7 @@
 package com.eNoLJ.baseball.service;
 
 import com.eNoLJ.baseball.web.dto.GameEntryDTO;
+import com.eNoLJ.baseball.web.dto.MemberDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,5 +17,19 @@ public class GameService {
         gameEntryDTOList.add(new GameEntryDTO("Dodgers", "Rockets"));
         gameEntryDTOList.add(new GameEntryDTO("Pintos", "Heros"));
         return gameEntryDTOList;
+    }
+
+    public List<MemberDTO> getMembersByTeamName(String teamName) {
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        memberDTOList.add(new MemberDTO(1L, "김광진", 0.311));
+        memberDTOList.add(new MemberDTO(2L, "이동규", 0.322));
+        memberDTOList.add(new MemberDTO(3L, "김진수", 0.333));
+        memberDTOList.add(new MemberDTO(4L, "박영권", 0.344));
+        memberDTOList.add(new MemberDTO(5L, "추신수", 0.355));
+        memberDTOList.add(new MemberDTO(6L, "이용대", 0.366));
+        memberDTOList.add(new MemberDTO(7L, "류현진", 0.377));
+        memberDTOList.add(new MemberDTO(8L, "최동수", 0.388));
+        memberDTOList.add(new MemberDTO(9L, "한양범", 0.399));
+        return memberDTOList;
     }
 }
--- BE/src/main/java/com/eNoLJ/baseball/web/GameController.java
+++ BE/src/main/java/com/eNoLJ/baseball/web/GameController.java
@@ -2,9 +2,11 @@ package com.eNoLJ.baseball.web;
 
 import com.eNoLJ.baseball.service.GameService;
 import com.eNoLJ.baseball.web.dto.GameEntryDTO;
+import com.eNoLJ.baseball.web.dto.MemberDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,4 +26,12 @@ public class GameController {
         logger.info("게임 목록 요청");
         return gameService.getGameList();
     }
+
+    @GetMapping("/members/{teamName}")
+    public List<MemberDTO> viewMembers(@PathVariable String teamName) {
+        logger.info("{}팀의 선수 목록 요청", teamName);
+        return gameService.getMembersByTeamName(teamName);
+    }
+
+
 }
