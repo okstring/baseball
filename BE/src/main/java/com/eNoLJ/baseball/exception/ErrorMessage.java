67c147edc064cb216693d7fd1706cebaf422e1ab
jeong-inho <derosatam76@gmail.com>
2021-05-09 02:58:33 +0900

    [#20] Feat: TokenException 클래스 생성
    jwt 사용 시 기한이 만료 되었거나 디코딩이 되지 않을 때 사용할 exception 생성

--- BE/src/main/java/com/eNoLJ/baseball/exception/ErrorMessage.java
+++ BE/src/main/java/com/eNoLJ/baseball/exception/ErrorMessage.java
@@ -3,7 +3,8 @@ package com.eNoLJ.baseball.exception;
 public enum ErrorMessage {
 
     ENTITY_NOT_FOUND("해당 엔티티를 찾을 수 없습니다."),
-    OAUTH_FAILED("OAuth 인증에 실패 했습니다.");
+    OAUTH_FAILED("OAuth 인증에 실패 했습니다."),
+    INVALID_TOKEN("유효하지 않은 토큰입니다.");
 
     private final String errorMessage;
 
---
+++ BE/src/main/java/com/eNoLJ/baseball/exception/TokenException.java
@@ -0,0 +1,19 @@
+package com.eNoLJ.baseball.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class TokenException extends RuntimeException {
+
+    private ErrorMessage errorMessage;
+
+    public TokenException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public TokenException(ErrorMessage errorMessage) {
+        super(errorMessage.getErrorMessage());
+        this.errorMessage = errorMessage;
+    }
+}
