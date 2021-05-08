9407ffe5ba17e3f7c18f5aedc870e1e399063000
zeke <yangsin1031@naver.com>
2021-05-09 01:39:17 +0900

    [#19] Feat: 주루선을 그려넣은 CALayer 생성

--- iOS/baseball/baseball/ViewController/GameView.swift
+++ iOS/baseball/baseball/ViewController/GameView.swift
@@ -11,15 +11,17 @@ import SnapKit
 class GameView: UIView {
     
     private var baseLayer = CAShapeLayer()
-    private var baseLineLayer = CALayer()
+    private var baseLineLayer = CAShapeLayer()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUpBaseLineLayer()
         setUpBaseLayer()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setUpBaseLineLayer()
         setUpBaseLayer()
     }
     
@@ -65,31 +67,22 @@ class GameView: UIView {
         self.layer.addSublayer(baseLayer)
     }
     
-    
-//    override func draw(_ rect: CGRect) {
-//        let path = UIBezierPath()
-//        let path2 = UIBezierPath()
-//        path.lineWidth = 2
-//
-//        // 이동경로 그리기
-//        path.move(to: CGPoint(x: self.frame.width / 2, y: self.frame.height - 50))
-//        path.addLine(to: CGPoint(x: self.frame.width/2 + 115, y: self.frame.height/2 ))
-//        path.addLine(to: CGPoint(x: self.frame.width/2, y: 50))
-//        path.addLine(to: CGPoint(x: self.frame.width/2 - 115, y: self.frame.height/2))
-//        path.close()
-//        UIColor.systemGray.set()
-//        path.stroke()
-//        
-//        // 홈베이스 그리기
-//        path2.move(to: CGPoint(x: 207, y: 248))
-//        path2.addLine(to: CGPoint(x: 196, y: 268))
-//        path2.addLine(to: CGPoint(x: 196, y: 286))
-//        path2.addLine(to: CGPoint(x: 218, y: 286))
-//        path2.addLine(to: CGPoint(x: 218, y: 268))
-//        path2.close()
-//
-//        UIColor.systemBackground.set()
-//        path2.fill()
-//
-//    }
+    func setUpBaseLineLayer() {
+        let path = UIBezierPath()
+        let centerX = CGFloat(bounds.size.width/2)
+        let centerY = CGFloat(bounds.size.height/2)
+        
+        path.move(to: CGPoint(x: centerX, y: centerY+100))
+        path.addLine(to: CGPoint(x: centerX+100, y: centerY))
+        path.addLine(to: CGPoint(x: centerX, y: centerY-100))
+        path.addLine(to: CGPoint(x: centerX-100, y: centerY))
+        path.close()
+        
+        baseLineLayer.frame = self.bounds
+        baseLineLayer.path = path.cgPath
+        baseLineLayer.lineWidth = 2
+        baseLineLayer.fillColor = .none
+        baseLineLayer.strokeColor = UIColor.gray.cgColor
+        self.layer.addSublayer(baseLineLayer)
+    }
 }
